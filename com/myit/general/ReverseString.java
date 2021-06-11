ef22c09bec26f15be25ab7d8d4458ae69c4caea8
sankarinfo@gmail.com <sankarinfo@gmail.com>
2021-06-11 10:07:20 +0800

    Reverse String

---
+++ com/myit/general/ReverseString.java
@@ -0,0 +1,31 @@
+package com.myit.general;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String reverse = "testing";
+		
+		StringBuilder sb = new StringBuilder(reverse);
+		System.out.println(sb.reverse().toString());
+		
+		new ReverseString().reverseString();
+		
+	}
+	
+	
+	public void reverseString() {
+	    String alphabet = "testing";
+	    String reverseAlphabet = reverse(alphabet, alphabet.length()-1);
+	    System.out.println("reversed string "+reverseAlphabet);
+	}
+
+	String reverse(String stringToReverse, int index){
+	    if(index == 0){
+	        return stringToReverse.charAt(0) + "";
+	    }
+
+	    char letter = stringToReverse.charAt(index);
+	    return letter + reverse(stringToReverse, index-1);
+	}
+
+}
